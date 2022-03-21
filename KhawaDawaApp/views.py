165c144e167623930caa9b0d0698c66d0cfb1bfe
Md Ariful islam <32597520+Arifcse03@users.noreply.github.com>
2022-03-21 08:22:31 +0600

    update  function fixed

--- KhawaDawaApp/views.py
+++ KhawaDawaApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from KhawaDawaApp.forms import KhawadawaForm
 from KhawaDawaApp.models import Khawadawa
 from django.contrib import messages
@@ -32,23 +32,41 @@ def edit(request, id):
     return render(request, 'edit.html', {'KhawaDawa': employee})
 
 
-def update(request, id):
-    logger = logging.getLogger('django')
-    employee = Khawadawa .objects.get(id=id)
-    form = KhawadawaForm(request.POST, instance=employee)
+# def update(request, id):
+#     logger = logging.getLogger('django')
+#     employee = Khawadawa .objects.get(id=id)
+#     form = KhawadawaForm(request.POST, instance=employee)
 
-    logger.info('before update validation check')
+#     logger.info('before update validation check')
 
-    if form.is_valid():
-        logger = logging.getLogger('django')
-        logger.info('after validation message')
+#     if form.is_valid():
+#         logger = logging.getLogger('django')
+#         logger.info('after validation message')
 
-        form.save()
-        return redirect('/api/show')
-    return render(request, 'edit.html', {'KhawaDawa': employee})
+#         form.save()
+#         return redirect('/api/show')
+#     return render(request, 'edit.html', {'KhawaDawa': employee})
 
 
 def destroy(request, id):
     employee = Khawadawa .objects.get(id=id)
     employee.delete()
     return redirect('/api/show')
+
+def update(request, id):
+    id = int(id)
+    try:
+        print("inside try")
+        khawa = Khawadawa.objects.get(id = id)
+        print(khawa)
+
+    except Khawadawa.DoesNotExist:
+        print("inside except")
+        return redirect('/api/show')
+    khawa_form = KhawadawaForm(request.POST or None, instance = khawa)
+    if khawa_form.is_valid():
+       khawa_form.save()
+       print("inside  after save")
+       return redirect('/api/show')
+    return render(request, 'edit.html', {'KhawaDawa': khawa_form})
+    
